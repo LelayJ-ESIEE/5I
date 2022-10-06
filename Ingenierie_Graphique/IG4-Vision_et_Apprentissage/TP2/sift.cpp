581c2cc9d76ffa18468d9c953474a3da340e0d61
LelayJ-ESIEE <jules.lelay@edu.esiee.fr>
2022-10-06 09:32:06 +0200

    IG4 - TP2 - improve keypoints drawing readability

--- Ingenierie_Graphique/IG4-Vision_et_Apprentissage/TP2/sift.cpp
+++ Ingenierie_Graphique/IG4-Vision_et_Apprentissage/TP2/sift.cpp
@@ -9,7 +9,7 @@ using namespace cv;
 using namespace std;
 
 Mat src[2];
-std::vector<KeyPoint> kp[2];
+std::vector<KeyPoint> kpts[2];
 Mat descriptor[2];
 Mat dst[2];
 
@@ -31,12 +31,13 @@ int main( int argc, char** argv )
       
       // SIFT detection
       Ptr<Feature2D> ptr = cv::SIFT::create();
-      ptr->detectAndCompute(src[i], Mat(), kp[i], descriptor[i]);
+      ptr->detectAndCompute(src[i], Mat(), kpts[i], descriptor[i]);
 
       // Draw Keypoints
-      cv::drawKeypoints	( src[i], kp[i], dst[i], Scalar(0,170,0), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
+      cv::drawKeypoints	( src[i], kpts[i], dst[i], Scalar(0,192,0), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
 
       namedWindow(noms[i]);
+      // imshow(noms[i], src[i]);
       imshow(noms[i], dst[i]);
     }
   
