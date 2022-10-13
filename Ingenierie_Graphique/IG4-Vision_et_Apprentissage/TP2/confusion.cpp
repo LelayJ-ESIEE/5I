2f7a9e01df3275f92a6594cd651fb05ebefc1fa1
LelayJ-ESIEE <jules.lelay@edu.esiee.fr>
2022-10-13 10:49:01 +0200

    IG4 - TP2 - rework old main to double moyenne(...)

--- Ingenierie_Graphique/IG4-Vision_et_Apprentissage/TP2/confusion.cpp
+++ Ingenierie_Graphique/IG4-Vision_et_Apprentissage/TP2/confusion.cpp
@@ -17,7 +17,7 @@ Mat dst[2];
 std::vector<DMatch> matches;
 Mat img_matches;
 
-int main( int argc, char** argv )
+double moyenne( int argc, char** argv )
 {
   Mat tmp;
   char *noms[] = { "source", "cible" };
@@ -38,7 +38,7 @@ int main( int argc, char** argv )
     ptr->detectAndCompute(src[i], Mat(), kpts[i], descriptor[i]);
 
     // Draw Keypoints
-    drawKeypoints( src[i], kpts[i], dst[i], Scalar(0,192,0), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
+    // drawKeypoints( src[i], kpts[i], dst[i], Scalar(0,192,0), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
 
     // namedWindow(noms[i]);
     // imshow(noms[i], src[i]);
@@ -48,27 +48,32 @@ int main( int argc, char** argv )
   // Compute SIFTs correspondances
   Ptr<BFMatcher> BFMatcher = cv::BFMatcher::create();  
   BFMatcher->DescriptorMatcher::match(descriptor[0], descriptor[1], matches);
+
   // Sort pairs and print them
   sort(matches.begin(), matches.end());
   float avg = 0.0f;
-  for(int i = 0; i < matches.size(); i++)
+  for(int i = 0; i < matches.size() && i<N; i++)
   {
-    cout << "KP " << matches[i].queryIdx << " <-> " << matches[i].trainIdx << ", distance=" << matches[i].distance << "\n";
+    // cout << "KP " << matches[i].queryIdx << " <-> " << matches[i].trainIdx << ", distance=" << matches[i].distance << "\n";
     if(i<N)
       avg += matches[i].distance;
   }
   avg /= N;
+
   // Show the two images with the N best correspondances
+  /* 
   vector<char> mask(matches.size(), 0);
   for(int i; i < N; i++)
     mask[i] = 1;
   namedWindow("figure");
   drawMatches(src[0], kpts[0], src[1], kpts[1], matches, img_matches, Scalar(0,192,0), Scalar(0,192,0), mask, DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
   imshow("figure", img_matches);
-  // Print statistics
-  cout << "STATISTIQUES: moyenne=" << avg << ", min=" << matches[0].distance << ", max=" << matches[N-1].distance << "\n";
+  */
 
-  waitKey();
-  return 0;
+  // Print statistics
+  // cout << "STATISTIQUES: moyenne=" << avg << ", min=" << matches[0].distance << ", max=" << matches[N-1].distance << "\n";
+
+  // waitKey();
+  return avg;
 }
 
