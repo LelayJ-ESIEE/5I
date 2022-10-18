f1ba3c3ff1d96a12ddcd6e3890c432df7cb769b6
LelayJ-ESIEE <jules.lelay@edu.esiee.fr>
2022-10-18 15:00:47 +0200

    IN9 - TP5 - Add Functions table

---
+++ INformatique/IN9-Developpement_web_back-end/TP/esiee.tp5/src/main/java/com/esiee/tp/domain/Function.java
@@ -0,0 +1,52 @@
+package com.esiee.tp.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "function")
+public class Function implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "code")
+	private String code;
+	
+	@Column(name = "label")
+	private String label;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	
+}
