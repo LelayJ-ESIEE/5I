81b91636a05ddea64d8b164ae4c1955a10b2f94f
LelayJ-ESIEE <jules.lelay@edu.esiee.fr>
2022-10-18 15:04:46 +0200

    IN9 - TP5 - Create FunctionRepository

---
+++ INformatique/IN9-Developpement_web_back-end/TP/esiee.tp5/src/main/java/com/esiee/tp/repository/FunctionRepository.java
@@ -0,0 +1,9 @@
+package com.esiee.tp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.esiee.tp.domain.Function;
+
+public interface FunctionRepository extends JpaRepository<Function, Long> {
+
+}
