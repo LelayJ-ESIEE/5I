3939624b5c0e15b857e793c38b81915d74896a05
LelayJ-ESIEE <jules.lelay@edu.esiee.fr>
2022-10-19 09:59:03 +0200

    IG4 - TP3 - Début d'implémentation de vocabulaire

--- Ingenierie_Graphique/IG4-Vision_et_Apprentissage/TP3/vect.py
+++ Ingenierie_Graphique/IG4-Vision_et_Apprentissage/TP3/vect.py
@@ -1,20 +1,58 @@
-from unittest import main
+from glob import glob
 import cv2
+import sklearn.cluster
+import numpy as np
+
 ## 1.3 Base d’images
 # ???
 
 ### 2.1.1 Fonction vocabulaire
-def vocabulaire(N: int, chemins: list(""), fichier: str, methode: str):
+def vocabulaire(N: int, chemins: list(""), fichier: str = "", methode: str = "agglomerative_clustering"):
     """
     Etant donne un entier N, une liste des chemins d’acces a des repertoires, et eventuellement un parametre pour la methode a utiliser :
     — Charge chaque image de ces repertoires, et en extrait des SIFT. Aide : glob.glob, s=cv2.xfeatures2d.SIFT_create(), s.detectAndCompute()
     — Clusterise l’ensemble des SIFT trouves en N clusters, en tenant compte de methode ou non. Aide : sklearn.cluster.*
     — Sauvegarde sur disque les centres de clusters SIFT trouves sous forme d’une matrice, que nous appellerons matrice vocabulaire, si fichier est precise. Aide : savetxt().
-    — Retourne l’inertie moyenne, et la plus grande erreur au sens de la norme L2 induites par cette clusterisation.
+    — Retourne a minima l’inertie moyenne, et la plus grande erreur au sens de la norme L2 induites par cette clusterisation.
     """
+    imgs_path = []
     for chemin in chemins:
-        img = cv2.imread(chemin)
+        imgs_path += [img for img in glob(chemin + "/*")]
+    
+    inertie = 0.0
+    
+    for path in imgs_path:
+        img = cv2.imread(path)
         s=cv2.SIFT_create()
+        [keypoints, descriptors] = s.detectAndCompute(img, None)
+
+        centers = None
+
+        if methode == "hierarchical_clustering":
+            ag = sklearn.cluster.AgglomerativeClustering(n_clusters=N)
+            ag.fit(descriptors)
+            centers = ag.distances_
+            # inertie += # TODO
+            print(centers)
+        elif methode == "mbkmeans":
+            mbkmeans = sklearn.cluster.MiniBatchKMeans(n_clusters=N)
+            mbkmeans.fit(descriptors)
+            centers = mbkmeans.cluster_centers_
+            inertie += mbkmeans.inertia_
+            print(centers)
+        elif methode == "kmeans":
+            kmeans = sklearn.cluster.KMeans(n_clusters=N)
+            kmeans.fit(descriptors)
+            centers = kmeans.cluster_centers_
+            inertie += kmeans.inertia_
+            print(centers)
+        else:
+            raise NotImplementedError
+        
+        if fichier != "":
+            np.savetxt(fichier, centers)
+
+    return inertie/len(imgs_path)
 
 ### 2.1.2 Recherche de N
 def coude():
@@ -62,4 +100,4 @@ def test_vecto():
 # ???
 
 if __name__ == "__main__":
-    pass
+    print(vocabulaire(1, ["airplanes/test", "ant/test", "dolphin/test", "wild_cat/test"], "./test"))
