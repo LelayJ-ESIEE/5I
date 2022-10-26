b889be0acf358ac3dbcae978a9b6ac77f236296c
LelayJ-ESIEE <jules.lelay@edu.esiee.fr>
2022-10-26 14:46:03 +0200

    IG4-TP4 - Homographie

--- Ingenierie_Graphique/IG4-Vision_et_Apprentissage/TP4/trump.cpp
+++ Ingenierie_Graphique/IG4-Vision_et_Apprentissage/TP4/trump.cpp
@@ -27,17 +27,40 @@ int main( int, char** argv )
 
   
   // Les afficher
-  //namedWindow( win_name, CV_WINDOW_AUTOSIZE );
-  //imshow( win_name, panneau );
+  // namedWindow( win_name, WINDOW_AUTOSIZE );
+  // imshow( win_name, panneau );
+  // setMouseCallback("panneau", mouse_callback, NULL);
   vector<Point2f> psrc, pdst;
 
-  // A compléter
+  /* On préférera la fonction findHomography() car nous avons 4 points à projeter, getAffineTransform() étant limité à 3 paires de points pour le calcul */
 
+  // A compléter
+  psrc.push_back(Point2f(0, 0));
+  psrc.push_back(Point2f(trump.cols, 0));
+  psrc.push_back(Point2f(trump.cols, trump.rows));
+  psrc.push_back(Point2f(0, trump.rows));
+
+  pdst.push_back(Point2f(498, 96));
+  pdst.push_back(Point2f(727, 75));
+  pdst.push_back(Point2f(763, 464));
+  pdst.push_back(Point2f(466, 486));
+
+  Mat M = findHomography(psrc, pdst);
+  Mat resultat;
+  warpPerspective(trump, resultat, M, Size(panneau.cols, panneau.rows));
+
+  for(int j = 0; j < resultat.rows; j++){
+    for(int i = 0; i < resultat.cols; i++){
+      if(resultat.at<Vec3b>(j,i) == Vec3b(0,0,0)){
+        resultat.at<Vec3b>(j,i) = panneau.at<Vec3b>(j,i);
+      }
+    }
+  }
 
   namedWindow("resultat", WINDOW_AUTOSIZE );
-  imshow("resultat", panneau );
+  imshow("resultat", resultat );
 
-  imwrite("resultat.jpg", panneau);
+  imwrite("resultat.jpg", resultat);
   // Attendre une touche 
   waitKey(0);
 
