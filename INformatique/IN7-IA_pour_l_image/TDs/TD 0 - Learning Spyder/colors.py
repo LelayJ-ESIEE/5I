b5229e17696b314d37c22dd613163273f64ed4c5
LelayJ-ESIEE <jules.lelay@edu.esiee.fr>
2023-01-15 16:14:28 +0100

    IN7 - TD0 - Introduction Spyder - little fixes

--- INformatique/IN7-IA_pour_l_image/TDs/TD 0 - Learning Spyder/colors.py
+++ INformatique/IN7-IA_pour_l_image/TDs/TD 0 - Learning Spyder/colors.py
@@ -1,14 +1,13 @@
 import cv2
 import numpy as np
 
-colors = ["Red", "Green", "Blue"]
-for c in range(3):
+colors = ["Blue", "Green", "Red"]
+for c in range(-1,-3,-1):
     img = np.zeros((512,512,3),np.uint8)
     for i in range(512):
         for j in range(512):
             img[i][j][c] = 255
+    print(img) # pour afficher dans la console
     cv2.imshow(colors[c],img)
     cv2.waitKey(0)
-
-for i in range(3):
-    cv2.destroyWindow(colors[i])
+    cv2.destroyWindow(colors[c])
